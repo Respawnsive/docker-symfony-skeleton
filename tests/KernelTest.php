03faf57dceac3a7288a268c8ee740f2c8547ce60
davidnurdin <david.nurdin@respawnsive.com>
2023-05-24 11:04:13 +0200

    Simple test

---
+++ tests/KernelTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Tests;
+
+use App\Kernel;
+use Doctrine\DBAL\Connection;
+use PHPUnit\Exception;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class KernelTest extends KernelTestCase
+{
+    public function testKernel(): void
+    {
+        // (1) boot the Symfony kernel
+        self::bootKernel();
+
+        // (2) use static::getContainer() to access the service container
+        $container = static::getContainer();
+
+        $kernel = $container->get(Kernel::class);
+        $this->assertTrue($kernel instanceof Kernel);
+    }
+
+    public function testDatabase(): void
+    {
+        // (1) boot the Symfony kernel
+        self::bootKernel();
+
+        // (2) use static::getContainer() to access the service container
+        $container = static::getContainer();
+
+        /** @var Connection $dbal */
+        $dbal = $container->get(Connection::class) ;
+
+        try {
+            $dbal->connect() ;
+        }
+        catch(\Doctrine\DBAL\Exception $ex)
+        {
+
+        }
+
+        $this->assertTrue($dbal->isConnected()) ;
+    }
+}
